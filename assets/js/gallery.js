13494f7892eafd6cd5ad32c38dcda4be9599effe
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-05-23 20:13:43 +0100

    Initial modal logic developed

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,1 +1,15 @@
 console.log("Chips")
+
+// MODAL LOGIC
+
+const openMovie = document.getElementById('movie-modal-open');
+const closeMovie = document.getElementById('movie-modal-close');
+const movieModal = document.getElementById('movie-modal');
+
+openMovie.addEventListener("click", () => {
+    movieModal.showModal();
+})
+
+closeMovie.addEventListener("click", () => {
+    movieModal.close();
+})
