48102f884ba2e303bb4fd798be1a9e8542bdd603
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-05-23 20:25:38 +0100

    Basic modals developed

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -5,6 +5,15 @@ console.log("Chips")
 const openMovie = document.getElementById('movie-modal-open');
 const closeMovie = document.getElementById('movie-modal-close');
 const movieModal = document.getElementById('movie-modal');
+const openWordle = document.getElementById('wordle-modal-open');
+const closeWorldle = document.getElementById('wordle-modal-close');
+const wordleModal = document.getElementById('wordle-modal');
+const openPortfolio = document.getElementById('portfolio-modal-open');
+const closePortfolio = document.getElementById('portfolio-modal-close');
+const portfolioModal = document.getElementById('portfolio-modal');
+const openTeam = document.getElementById('team-modal-open');
+const closeTeam = document.getElementById('team-modal-close');
+const teamModal = document.getElementById('team-modal');
 
 openMovie.addEventListener("click", () => {
     movieModal.showModal();
@@ -12,4 +21,28 @@ openMovie.addEventListener("click", () => {
 
 closeMovie.addEventListener("click", () => {
     movieModal.close();
+})
+
+openWordle.addEventListener("click", () => {
+    wordleModal.showModal();
+})
+
+closeWorldle.addEventListener("click", () => {
+    wordleModal.close();
+})
+
+openPortfolio.addEventListener("click", () => {
+    portfolioModal.showModal();
+})
+
+closePortfolio.addEventListener("click", () => {
+    portfolioModal.close();
+})
+
+openTeam.addEventListener("click", () => {
+    teamModal.showModal();
+})
+
+closeTeam.addEventListener("click", () => {
+    teamModal.close();
 })
