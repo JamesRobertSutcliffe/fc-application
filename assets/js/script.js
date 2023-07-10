17d118ecadf12ced71966250148bed879fa476db
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-07-10 22:25:48 +0100

    Toggle switcher working, update button and then move to other pages next

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-console.log("hello world")
+const switchBtn = document.getElementById('color-toggle');
+const imgBack = document.querySelectorAll('#img-back');
+
+// Function to toggle the color theme
+function toggleTheme() {
+    const body = document.body;
+    const root = document.documentElement;
+
+    // Toggle the values of CSS variables
+    if (body.classList.contains('dark-theme')) {
+        root.style.setProperty('--primary-color', 'rgb(252, 252, 212)');
+        imgBack.forEach(i => {
+            i.classList.remove('img-back');
+        })
+        body.classList.remove('dark-theme');
+    } else {
+        root.style.setProperty('--primary-color', 'rgb(10, 68, 61)');
+        imgBack.forEach(i => {
+            i.classList.add('img-back');
+        })
+        body.classList.add('dark-theme');
+    };
+};
+
+// Event listener toggles themes
+switchBtn.addEventListener('click', toggleTheme);
