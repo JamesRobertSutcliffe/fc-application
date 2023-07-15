de537ed4ceba187824d3177b2b8b295d88094e3e
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-07-15 14:45:38 +0100

    Color switch logic completed

--- README.md
+++ README.md
@@ -6,18 +6,9 @@ to do now:
 5 - Read me for application game
 6 - Application form
 
-Possibly - 
-
-Website - 
-- Colour switcher
-
 GAME:
 - add testing for functions? (testing in vanilla JS)
 
-Colour switchers -
-
-1 - build button aestetics for switcher - next!
-2 - transfer logic to other pages
 3 - evidence of testing in JS 
 
 !! Ensure I understand all code !!
--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -73,6 +73,31 @@ closeWorldle.addEventListener("click", () => {
     wordleModal.close();
 })
 
+// Colour Switch Logic 
+
+const switchBtn = document.getElementById('color-toggle');
+const imgBack = document.querySelectorAll('#img-back');
+
+// Function to toggle the color theme
+function toggleTheme() {
+    const body = document.body;
+
+    // Toggle the values of CSS variables
+    if (body.classList.contains('dark-theme')) {
+        imgBack.forEach(i => {
+            i.classList.remove('img-back');
+        })
+        body.classList.remove('dark-theme');
+    } else {
+        imgBack.forEach(i => {
+            i.classList.add('img-back');
+        })
+        body.classList.add('dark-theme');
+    };
+};
+
+// Event listener toggles themes
+switchBtn.addEventListener('click', toggleTheme);
 
 
 
